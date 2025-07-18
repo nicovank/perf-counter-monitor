70bfafb9c91b063b7abf7baa9059d5fd03513b53
Nicolas van Kempen <nvankemp@gmail.com>
2025-07-18 15:29:59 -0400

    Add dTLB cache miss rate

--- src/main.cpp
+++ src/main.cpp
@@ -9,10 +9,12 @@
 #include <cstring>
 #include <deque>
 #include <exception>
+#include <initializer_list>
 #include <iostream>
 #include <numeric>
 #include <string>
 #include <thread>
+#include <unordered_map>
 #include <utility>
 #include <vector>
 
@@ -49,25 +51,33 @@ std::string humanReadable(std::uint64_t value) {
   return std::to_string(value / 1'000'000'000'000) + "T";
 }
 
+std::uint64_t sum_over_dims(const std::vector<std::uint64_t> &data,
+                            std::initializer_list<std::size_t> dims) {
+  return std::accumulate(
+      dims.begin(), dims.end(), std::uint64_t(0),
+      [&data](std::uint64_t sum, std::size_t dim) { return sum + data[dim]; });
+}
+
 ftxui::Element
 plot_single_value(const std::deque<std::vector<std::uint64_t>> &data,
+                  std::initializer_list<std::size_t> dims,
                   std::uint64_t period_ms) {
   auto c = ftxui::Canvas(150, 100);
 
   const int mx = 10; // Start plotting from this x value.
-  const int my = 5;  // Start plotting from this y value.
+  const int my = 8;  // Start plotting from this y value.
 
-  const std::uint64_t max_value =
-      std::accumulate(data.begin(), data.end(), std::uint64_t(0),
-                      [](std::uint64_t a, const std::vector<std::uint64_t> &b) {
-                        return std::max(a, b[0]);
-                      });
+  const std::uint64_t max_value = std::accumulate(
+      data.begin(), data.end(), std::uint64_t(0),
+      [dims](std::uint64_t a, const std::vector<std::uint64_t> &b) {
+        return std::max(a, sum_over_dims(b, dims));
+      });
 
   std::vector<int> xs;
   std::vector<int> ys;
 
   for (std::size_t i = 0; i < data.size(); ++i) {
-    const auto value = data[i][0];
+    const auto value = sum_over_dims(data[i], dims);
 
     const auto x = static_cast<int>(static_cast<double>(i) /
                                         static_cast<double>(data.size()) *
@@ -88,7 +98,53 @@ plot_single_value(const std::deque<std::vector<std::uint64_t>> &data,
   }
 
   c.DrawText(0, 0, humanReadable(max_value));
-  c.DrawText(6, 95, "0");
+  c.DrawText(0, 95, "0");
+  c.DrawText(10, 99,
+             "-" + std::to_string(data.size() * period_ms / 1'000) + "s");
+  c.DrawText(149, 99, "0");
+  c.DrawBlockLine(10, 0, 10, 95, ftxui::Color::GrayDark);
+  c.DrawBlockLine(10, 95, 150, 95, ftxui::Color::GrayDark);
+
+  return canvas(std::move(c));
+}
+
+ftxui::Element plot_ratio(const std::deque<std::vector<std::uint64_t>> &data,
+                          std::initializer_list<std::size_t> xDims,
+                          std::initializer_list<std::size_t> yDims,
+                          std::uint64_t period_ms) {
+  auto c = ftxui::Canvas(150, 100);
+
+  const int mx = 10; // Start plotting from this x value.
+  const int my = 8;  // Start plotting from this y value.
+
+  std::vector<int> xs;
+  std::vector<int> ys;
+
+  for (std::size_t i = 0; i < data.size(); ++i) {
+    const auto numerator = static_cast<double>(sum_over_dims(data[i], xDims));
+    const auto denominator = static_cast<double>(sum_over_dims(data[i], yDims));
+
+    const auto value = denominator == 0 ? 0 : (numerator / denominator * 100.0);
+
+    const auto x = static_cast<int>(static_cast<double>(i) /
+                                        static_cast<double>(data.size()) *
+                                        (c.width() - mx) +
+                                    mx);
+    const auto y = static_cast<int>(
+        c.height() -
+        (static_cast<double>(value) / 100.0 * (c.height() - my) + my));
+
+    xs.push_back(x);
+    ys.push_back(y);
+  }
+
+  c.DrawPointLine(xs[0], ys[0], xs[1], ys[1], ftxui::Color::Green);
+  for (int i = 1; i < xs.size() - 1; ++i) {
+    c.DrawPointLine(xs[i], ys[i], xs[i + 1], ys[i + 1], ftxui::Color::Green);
+  }
+
+  c.DrawText(0, 0, "100%");
+  c.DrawText(0, 95, "0%");
   c.DrawText(10, 99,
              "-" + std::to_string(data.size() * period_ms / 1'000) + "s");
   c.DrawText(149, 99, "0");
@@ -102,7 +158,14 @@ ftxui::Element plot(const std::string &event,
                     const std::deque<std::vector<std::uint64_t>> &data,
                     std::uint64_t period_ms) {
   if (event == "cpu-cycles") {
-    return plot_single_value(data, period_ms);
+    return plot_single_value(data, {0}, period_ms);
+  }
+
+  if (event == "dtlb-miss-rate") {
+    return ftxui::vbox({
+        plot_ratio(data, {0, 1}, {2, 3}, period_ms) | ftxui::border,
+        plot_single_value(data, {2, 3}, period_ms) | ftxui::border,
+    });
   }
 
   std::unreachable();
@@ -123,13 +186,14 @@ int main(int argc, char **argv) {
   program.add_description(
       "Monitor a given performance counter over time, machine-wide.\n"
       "Available arguments for the --event/-e options are:\n"
-      " 1. cpu-cycles");
+      " 1. cpu-cycles\n"
+      " 2. dtlb-miss-rate");
 
   program.add_argument("-e", "--event")
       .required()
       .help("the name of the performance event to monitor, see -h for options")
       .metavar("EVENT")
-      .choices("cpu-cycles");
+      .choices("cpu-cycles", "dtlb-miss-rate");
 
   program.add_argument("-p", "--period-ms")
       .default_value(std::uint64_t(100))
@@ -158,11 +222,33 @@ int main(int argc, char **argv) {
   if (event == "cpu-cycles") {
     perfEventDescriptors.emplace_back(PERF_TYPE_HARDWARE,
                                       PERF_COUNT_HW_CPU_CYCLES);
+  } else if (event == "dtlb-miss-rate") {
+    perfEventDescriptors.insert(
+        perfEventDescriptors.end(),
+        {{PERF_TYPE_HW_CACHE, PERF_COUNT_HW_CACHE_DTLB |
+                                  (PERF_COUNT_HW_CACHE_OP_READ << 8) |
+                                  (PERF_COUNT_HW_CACHE_RESULT_MISS << 16)},
+         {PERF_TYPE_HW_CACHE, PERF_COUNT_HW_CACHE_DTLB |
+                                  (PERF_COUNT_HW_CACHE_OP_WRITE << 8) |
+                                  (PERF_COUNT_HW_CACHE_RESULT_MISS << 16)},
+         {PERF_TYPE_HW_CACHE, PERF_COUNT_HW_CACHE_DTLB |
+                                  (PERF_COUNT_HW_CACHE_OP_READ << 8) |
+                                  (PERF_COUNT_HW_CACHE_RESULT_ACCESS << 16)},
+         {PERF_TYPE_HW_CACHE, PERF_COUNT_HW_CACHE_DTLB |
+                                  (PERF_COUNT_HW_CACHE_OP_WRITE << 8) |
+                                  (PERF_COUNT_HW_CACHE_RESULT_ACCESS << 16)},
+         {PERF_TYPE_HARDWARE, PERF_COUNT_HW_CACHE_MISSES},
+         {PERF_TYPE_HARDWARE, PERF_COUNT_HW_CACHE_REFERENCES}});
   }
 
   perf::SystemWideGroup group(perfEventDescriptors);
 
-  std::cout << "Monitoring CPU cycles every " << period_ms
+  const auto nameByEvent = std::unordered_map<std::string, std::string>{
+      {"cpu-cycles", "CPU cycles"},
+      {"dtlb-miss-rate", "dTLB miss rate"},
+  };
+
+  std::cout << "Monitoring " << nameByEvent.at(event) << " every " << period_ms
             << " milliseconds. Press Ctrl+C or 'q' to stop." << std::endl;
 
   std::atomic_bool running = true;
